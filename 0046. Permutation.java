20d66f18332dd4b6ee2866ba95bc361610f40e04
Dhanyashree HS <dhanyashreesgurukar@gmail.com>
2025-12-09 16:08:14 +0530

    Initial commit

--- 0046. Permutation.java
+++ 0046. Permutation.java
@@ -1,4 +1,4 @@
-/*Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.*/
+/*Problem statement - Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.*/
 
 /*Sample test cases:
     Example 1:
